d96628fbf701c05281e23f6af611ef0898a3c5af
Matthew Rocklin <mrocklin@gmail.com>
2012-08-23 09:49:09 -0500

    add reverse_dict and merge to util.py

--- test_util.py
+++ test_util.py
@@ -9,3 +9,12 @@ def test_dictify():
     f = lambda a,b: a+b
     g = dictify(f)
     assert g[1,2] == 3
+
+def test_reverse_dict():
+    d = {'a': (1, 2), 'b': (2, 3), 'c':()}
+    assert reverse_dict(d) ==  {1: ('a',), 2: ('a', 'b'), 3: ('b',)}
+
+def test_merge():
+    d = {1:2, 3:4}
+    e = {4:5}
+    assert merge(d, e) == {1:2, 3:4, 4:5}
--- util.py
+++ util.py
@@ -24,3 +24,19 @@ def dictify(f):
     3
     """
     return fakedict(f)
+
+def reverse_dict(d):
+    """ Reverses direction of dependence dict
+
+    >>> d = {'a': (1, 2), 'b': (2, 3), 'c':()}
+    >>> reverse_dict(d)
+    {1: ('a',), 2: ('a', 'b'), 3: ('b',)}
+    """
+    result = {}
+    for key in d:
+        for val in d[key]:
+            result[val] = result.get(val, tuple()) + (key, )
+    return result
+
+def merge(d, e):
+    return dict(d.items() + e.items())
