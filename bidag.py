e8f46e2b1c026019ed6baa0092e905e8e02c4917
Matthew Rocklin <mrocklin@gmail.com>
2012-08-23 11:53:33 -0500

    add unidag_to_subbidiag

--- bidag.py
+++ bidag.py
@@ -7,18 +7,16 @@
 
 # We expect incoming bipartite dags as two dependency dicts
 
-def reverse_dict(d):
-    """ Reverses direction of dependence dict
+from util import reverse_dict, intersection
 
-    >>> d = {'a': (1, 2), 'b': (2, 3)}
-    >>> reverse_dict(d)
-    {1: ('a',), 2: ('a', 'b'), 3: ('b',)}
-    """
-    result = {}
-    for key in d:
-        for val in d[key]:
-            result[val] = result.get(val, tuple()) + (key, )
-    return result
+def send(*args):
+    return ("send",) + args
+def issend(x):
+    return isinstance(x, tuple) and x[0] == 'send'
+def recv(*args):
+    return ("recv",) + args
+def isrecv(x):
+    return isinstance(x, tuple) and x[0] == 'recv'
 
 def bidag_to_unidag(usedby, outputsof):
     """ Converts a bivariate DAG of jobs+variables to a dag of jobs
@@ -35,6 +33,41 @@ def bidag_to_unidag(usedby, outputsof):
     return {job: tuple(sum([usedby[output] for output in outputsof[job]
                                            if output in usedby], tuple()))
                for job in outputsof.keys()}
+
+
+def unidag_to_subbidag((usedby, outputsof), unidag):
+    """ Converts a bivariate DAG of jobs+variables to a dag of jobs
+
+    inputs:
+        usedby      - dict mapping variables to jobs that use them
+        outputsof   - dict mapping jobs to variables that they produce
+
+    >>> # 'a' -> 1 -> 'b' -> 2 -> c
+    >>> unidag_to_bidag({1: (2,)}, {'a': (1, ), 'b':(2, )},)
+    {1: (2, ), 2: tuple()}
+    """
+    inputsof = reverse_dict(usedby)
+    jobs = unidag.keys()
+    sends = filter(issend, jobs)
+    recvs = filter(isrecv, jobs)
+    small_usedby = {var: intersection(usedby[var], jobs)
+        for job in jobs
+        for var in inputsof.get(job, ())}
+    small_outputsof = {job: outputsof[job] for job in intersection(outputsof, jobs)}
+    print "inside unidag_to_subdiag"
+    print small_usedby
+    print small_outputsof
+
+    for (_, from_machine, to_machine, from_job, to_job) in sends:
+        for var in intersection(outputsof[from_job], inputsof[to_job]):
+            small_usedby[var] = small_usedby.get(var, ()) + (send(from_machine, to_machine),)
+        small_outputsof[send(from_machine, to_machine)] = ()
+
+    for (_, from_machine, to_machine, from_job, to_job) in recvs:
+        small_outputsof[recv(from_machine, to_machine)] = (
+                intersection(outputsof[from_job], inputsof[to_job]))
+
+    return small_usedby, small_outputsof
 
 def communication_conversion(usedby, outputsof, commcost):
     """ Convert a communication cost function from variables to jobs
--- test_bidag.py
+++ test_bidag.py
@@ -7,13 +7,48 @@ def test_reverse_dict():
     d = {'a': (1, 2), 'b': (2, 3)}
     assert reverse_dict(d) ==  {1: ('a',), 2: ('a', 'b'), 3: ('b',)}
 
-def test_bidag_to_unidag():
-    usedby = {'a': (1,), 'b': (2,)}
-    outputsof = {1: ('b',) , 2: ('c', )}
-    inputs = ('a', )
-    outputs = ('c', )
-    # 'a' -> 1 -> 'b' -> 2 -> c
+def test_bidag_to_unidag_simple():
+    usedby = {'a': (1,), 'b': (2,), 'c':(3, )}
+    outputsof = {1: ('b',) , 2: ('c', ), 3: ('d',)}
+    # a -> 1 -> b -> 2 -> c -> 3 -> d
 
     unidag = bidag_to_unidag(usedby, outputsof)
-    assert unidag == {1: (2, ), 2: tuple()}
+    assert unidag == {1: (2, ), 2: (3,), 3: ()}
 
+def test_bidag_to_unidag_less_simple():
+    usedby = {'a': (1,), 'b': (2,), 'c':(3, )}
+    outputsof = {1: ('b', 'c') , 2: ('d', ), 3: ('e',)}
+    # a -> 1 -> b -> 2 -> d
+    #        -> c -> 3 -> e
+
+    unidag = bidag_to_unidag(usedby, outputsof)
+    assert unidag == {1: (2, 3), 2: (), 3: ()}
+
+def test_unidag_to_subbidag():
+    usedby = {'a': (1,), 'b': (2,), 'c':(3, )}
+    outputsof = {1: ('b',) , 2: ('c', ), 3: ('d',)}
+    # a -> 1 -> b -> 2 -> c -> 3 -> d
+
+    subdag = {1: (2,), 2:(send('A', 'B', 2, 3),), send('A', 'B', 2, 3): ()}
+    # 1 -> 2 -> send(A, B, 2, 3)
+    sm_usedby, sm_outputsof = unidag_to_subbidag((usedby, outputsof), subdag)
+    assert sm_usedby    == {'a': (1,), 'b': (2,), 'c':(send('A', 'B'),)}
+    assert sm_outputsof == {1: ('b',), 2: ('c',), send('A', 'B'): ()}
+
+    subdag2 = {recv('A', 'B', 2, 3): (3,), 3: ()}
+    sm_usedby, sm_outputsof = unidag_to_subbidag((usedby, outputsof), subdag2)
+
+    print sm_usedby
+    print sm_outputsof
+    assert sm_usedby    == {'c': (3,)}
+    assert sm_outputsof == {recv('A', 'B'): ('c',), 3: ('d',)}
+
+def test_cycle():
+    usedby = {'a': (1,), 'b': (2,), 'c':(3, )}
+    outputsof = {1: ('b', 'c') , 2: ('d', ), 3: ('e',)}
+
+    unidag = bidag_to_unidag(usedby, outputsof)
+    usedby2, outputsof2 = unidag_to_subbidag((usedby, outputsof), unidag)
+
+    assert usedby == usedby2
+    assert outputsof == outputsof2
