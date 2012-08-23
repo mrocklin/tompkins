391b2bb29890280c3d70776d85f7350684462fe7
Matthew Rocklin <mrocklin@gmail.com>
2012-08-23 12:24:56 -0500

    add test for branching unidag-to-subdiag

--- test_bidag.py
+++ test_bidag.py
@@ -24,7 +24,7 @@ def test_bidag_to_unidag_less_simple():
     unidag = bidag_to_unidag(usedby, outputsof)
     assert unidag == {1: (2, 3), 2: (), 3: ()}
 
-def test_unidag_to_subbidag():
+def test_unidag_to_subbidag_simple():
     usedby = {'a': (1,), 'b': (2,), 'c':(3, )}
     outputsof = {1: ('b',) , 2: ('c', ), 3: ('d',)}
     # a -> 1 -> b -> 2 -> c -> 3 -> d
@@ -40,6 +40,37 @@ def test_unidag_to_subbidag():
 
     assert sm_usedby    == {'c': (3,)}
     assert sm_outputsof == {recv('A', 'B'): ('c',), 3: ('d',)}
+
+def test_unidag_to_subbidag_less_simple():
+    usedby = {'a': (1,), 'b': (2,), 'c':(3, )}
+    outputsof = {1: ('b', 'c') , 2: ('d', ), 3: ('e',)}
+    # a -> 1 -> b -> 2 -> d
+    #        -> c -> 3 -> e
+
+    unidagA = {1:(send('A', 'B', 1, 2), send('A', 'C', 1, 3)),
+               send('A', 'B', 1, 2): (),
+               send('A', 'C', 1, 3): ()}
+    sm_usedby, sm_outputsof = unidag_to_subbidag((usedby, outputsof), unidagA)
+    assert sm_usedby    == {'a': (1,),
+                            'b': (send('A', 'B'),),
+                            'c': (send('A',  'C'),)}
+    assert sm_outputsof == {1: ('b','c'),
+                            send('A', 'B'): (),
+                            send('A', 'C'): ()}
+
+    unidagB = {recv('A', 'B', 1, 2): (2,),
+               2: ()}
+    sm_usedby, sm_outputsof = unidag_to_subbidag((usedby, outputsof), unidagB)
+    assert sm_usedby    == {'b': (2,)}
+    assert sm_outputsof == {recv('A', 'B'): ('b',),
+                            2: ('d',)}
+
+    unidagC = {recv('A', 'C', 1, 3): (3,),
+               3: ()}
+    sm_usedby, sm_outputsof = unidag_to_subbidag((usedby, outputsof), unidagC)
+    assert sm_usedby    == {'c': (3,)}
+    assert sm_outputsof == {recv('A', 'C'): ('c',),
+                            3: ('e',)}
 
 def test_cycle():
     usedby = {'a': (1,), 'b': (2,), 'c':(3, )}
